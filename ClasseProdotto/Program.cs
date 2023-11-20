5762988346f13743952197aa4799c0f9bf9a845f
MirkoColo <colombomirko06@gmail.com>
2023-11-20 09:25:01 +0100

    Aggiungere i file di progetto.

---
+++ ClasseProdotto/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseProdotto
+{
+    class Prodotto
+    {
+        protected string _nome;
+        protected double _prezzo;
+        public Prodotto(string nome, double prezzo) 
+        { 
+            _nome = nome;
+            _prezzo = prezzo;
+        }
+        public Prodotto()
+        {
+
+            _nome = "Carne";
+            _prezzo = 15;
+        }
+
+        public void Stampa()
+        {
+            Console.WriteLine($"Il nome del prodotto è: {_nome} e il suo prezzo è: {_prezzo}");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Prodotto prod = new Prodotto("Farro", 10);
+            prod.Stampa();
+            Prodotto prod1 = new Prodotto();
+            prod1.Stampa();
+        }
+    }
+}
